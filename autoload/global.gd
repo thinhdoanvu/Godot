267e714a4c6c21f0a34ca04395e2dc4dc5c5f6bb
thinhdoanvu <thinhdv@ntu.edu.vn>
2024-02-20 08:56:34 +0700

    Add files via upload

---
+++ autoload/global.gd
@@ -0,0 +1,10 @@
+extends Node
+var hoten = ""
+var mssv = ""
+var gioithieu = false
+var tailieuhuongdan = false
+var thuchanh1 = false
+var thuchanh2 = false
+var tracnghiem = false
+var voigioithieu = true
+var diem = 0
